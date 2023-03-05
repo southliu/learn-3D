c26e363e1108cd75ead8c8623302ee9b801dbc4a
southliu <1275093225@qq.com>
2023-03-05 15:16:24 +0800

    feat: init

--- src/script.js
+++ src/script.js
@@ -8,69 +8,55 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-/**
- * Cursor
- */
-const cursor = {
-    x: 0,
-    y: 0
-}
-window.addEventListener('mousemove', even => {
-    cursor.x = - (even.clientX / sizes.width - 0.5)
-    cursor.y = even.clientY / sizes.height - 0.5
-    // console.log(cursor.x, cursor.y)
-})
+// Scene
+const scene = new THREE.Scene()
 
-// Sizes
+/**
+ * Object
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+/**
+ * Sizes
+ */
 const sizes = {
     width: 800,
     height: 600
 }
 
-// Scene
-const scene = new THREE.Scene()
-
-// Object
-const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-scene.add(mesh)
-
-// Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.01, 1199)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
-camera.lookAt(mesh.position)
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 3
 scene.add(camera)
 
-// Renderer
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 
-// Animate
-// const clock = new THREE.Clock()
-
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
-// controls.target .y = 1
-// controls.update()
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
 
 const tick = () =>
 {
-    // const elapsedTime = clock.getElapsedTime()
+    const elapsedTime = clock.getElapsedTime()
 
-    // Update objects
-    // mesh.rotation.y = elapsedTime;
-    // camera.position.x = cursor.x * 3
-    // camera.position.y = cursor.y * 3
-    // camera.lookAt(mesh.position)
-
-    // update controls
+    // Update controls
     controls.update()
 
     // Render
