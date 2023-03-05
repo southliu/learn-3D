25e12e1fa2779f9eb41f25d14956d59965ec8a8b
southliu <1275093225@qq.com>
2023-03-05 14:49:21 +0800

    feat: 物体跟踪鼠标旋转

--- src/script.js
+++ src/script.js
@@ -7,6 +7,19 @@ import * as THREE from 'three'
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
+/**
+ * Cursor
+ */
+const cursor = {
+    x: 0,
+    y: 0
+}
+window.addEventListener('mousemove', even => {
+    cursor.x = - (even.clientX / sizes.width - 0.5)
+    cursor.y = even.clientY / sizes.height - 0.5
+    // console.log(cursor.x, cursor.y)
+})
+
 // Sizes
 const sizes = {
     width: 800,
@@ -24,7 +37,7 @@ const mesh = new THREE.Mesh(
 scene.add(mesh)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.01, 1199)
 camera.position.x = 2
 camera.position.y = 2
 camera.position.z = 2
@@ -42,10 +55,13 @@ const clock = new THREE.Clock()
 
 const tick = () =>
 {
-    const elapsedTime = clock.getElapsedTime()
+    // const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    mesh.rotation.y = elapsedTime;
+    // mesh.rotation.y = elapsedTime;
+    camera.position.x = cursor.x * 3
+    camera.position.y = cursor.y * 3
+    camera.lookAt(mesh.position)
 
     // Render
     renderer.render(scene, camera)
