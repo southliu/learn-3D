44a8cb2b068ce56697971ab8c2a7884d02e42c3a
southliu <1275093225@qq.com>
2023-03-07 23:45:40 +0800

    feat: 3D文字

--- readme.md
+++ readme.md
@@ -1,26 +1,11 @@
 # 3D学习项目
 
-## Animate - 旋转动画
-```JS
-// Clock
-const clock = new THREE.Clock()
-
-// Animations
-const tick = () => {
-    // Clock
-    const elapsedTime = clock.getElapsedTime()
-    
-    mesh.position.x = Math.cos(elapsedTime)
-    mesh.position.y = Math.sin(elapsedTime)
-
-    // Render
-    renderer.render(scene, camera)
-
-    window.requestAnimationFrame(tick)
-}
-```
-
 # 性能优化
+
+## gltf压缩
+```
+https://gltf.report
+```
 
 ## 设置像素比
 ```js
@@ -44,9 +29,54 @@ const colorTexture = textureLoader.load('/textures/checkerboard-8x8.png')
 colorTexture.magFilter = THREE.NearestFilter
 ```
 
-## gltf压缩
+## 字体减少贝塞尔曲线
+```js
+const fontLoader = new FontLoader()
+fontLoader.load(
+    '/fonts/helvetiker_regular.typeface.json',
+    (font) => {
+        console.log('font loaded')
+        const textGeometry = new TextGeometry(
+            'Hello World',
+            {
+                font,
+                size: 0.5,
+                height: 0.2,
+                curveSegments: 12,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 3 // 减少贝塞尔曲线
+            }
+        )
+        const textMaterial = new THREE.MeshBasicMaterial()
+        textMaterial.wireframe = true
+        const textMesh = new THREE.Mesh(textGeometry, textMaterial)
+        textMesh.position.x = 1
+        scene.add(textMesh)
+    }
+)
 ```
-https://gltf.report
+
+## Animate - 旋转动画
+```JS
+// Clock
+const clock = new THREE.Clock()
+
+// Animations
+const tick = () => {
+    // Clock
+    const elapsedTime = clock.getElapsedTime()
+    
+    mesh.position.x = Math.cos(elapsedTime)
+    mesh.position.y = Math.sin(elapsedTime)
+
+    // Render
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
 ```
 
 # GUI
--- src/script.js
+++ src/script.js
@@ -2,6 +2,9 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
+import typefaceFont from 'three/examples/fonts/helvetiker_regular.typeface.json'
 
 /**
  * Base
@@ -19,16 +22,79 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const matcapTexture = textureLoader.load('/textures/matcaps/3.png')
+
+/**
+ * Font
+ */
+const fontLoader = new FontLoader()
+
+fontLoader.load(
+    '/fonts/helvetiker_regular.typeface.json',
+    (font) => {
+        console.log('font loaded')
+        const textGeometry = new TextGeometry(
+            'Hello World',
+            {
+                font,
+                size: 0.5,
+                height: 0.2,
+                curveSegments: 12,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 3
+            }
+        )
+        // textGeometry.computeBoundingBox()
+        // console.log(textGeometry.boundingBox)
+        // textGeometry.translate(
+        //     -textGeometry.boundingBox.max.x * 0.5,
+        //     -textGeometry.boundingBox.max.y * 0.5,
+        //     -textGeometry.boundingBox.max.z * 0.5,
+        // )
+        textGeometry.center()
+
+        const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+        // material.wireframe = true
+        const text = new THREE.Mesh(textGeometry, material)
+        // text.position.x = 1
+        scene.add(text)
+        
+        console.time('donut')
+
+        const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+
+        for (let i = 0; i < 100; i++) {
+            const donut = new THREE.Mesh(donutGeometry, material)
+
+            donut.position.x = (Math.random() - 0.5) * 10
+            donut.position.y = (Math.random() - 0.5) * 10
+            donut.position.z = (Math.random() - 0.5) * 10
+
+            donut.rotation.x = Math.random() * Math.PI
+            donut.rotation.y = Math.random() * Math.PI
+
+            const scale = Math.random()
+            donut.scale.set(scale, scale, scale)
+            
+            scene.add(donut)
+        }
+
+        console.timeEnd('donut')
+    }
+)
 
 /**
  * Object
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial()
+// )
 
-scene.add(cube)
+// scene.add(cube)
 
 /**
  * Sizes
