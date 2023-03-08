5f5ffa1bd456961b69b6006a5780b1fb4e6a08d6
southliu <1275093225@qq.com>
2023-03-08 19:02:52 +0800

    feat: init

--- readme.md
+++ readme.md
@@ -1,88 +1,16 @@
-# 3D学习项目
+# Three.js Journey
 
-# 性能优化
+## Setup
+Download [Node.js](https://nodejs.org/en/download/).
+Run this followed commands:
 
-## gltf压缩
+``` bash
+# Install dependencies (only the first time)
+npm install
+
+# Run the local server at localhost:8080
+npm run dev
+
+# Build for production in the dist/ directory
+npm run build
 ```
-https://gltf.report
-```
-
-## 设置像素比
-```js
-const renderer = new THREE.WebGLRenderer({ canvas })
-// 设置像素比
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-```
-
-## 处理纹理
-```js
-const loadingManager = new THREE.LoadingManager()
-const textureLoader = new THREE.TextureLoader(loadingManager)
-const colorTexture = textureLoader.load('/textures/checkerboard-8x8.png')
-/**
- * THREE.NearestFilter 最近点采样
- * THREE.LinearFilter 线性采样 默认值
- * minFilter 缩小滤镜（例如32X32的纹理映射到16X16的像素空间）
- * magFilter 放大滤镜（例如16X16的纹理映射到32X32的像素空间）
- */
-// colorTexture.minFilter = THREE.NearestFilter
-colorTexture.magFilter = THREE.NearestFilter
-```
-
-## 字体减少贝塞尔曲线
-```js
-const fontLoader = new FontLoader()
-fontLoader.load(
-    '/fonts/helvetiker_regular.typeface.json',
-    (font) => {
-        console.log('font loaded')
-        const textGeometry = new TextGeometry(
-            'Hello World',
-            {
-                font,
-                size: 0.5,
-                height: 0.2,
-                curveSegments: 12,
-                bevelEnabled: true,
-                bevelThickness: 0.03,
-                bevelSize: 0.02,
-                bevelOffset: 0,
-                bevelSegments: 3 // 减少贝塞尔曲线
-            }
-        )
-        const textMaterial = new THREE.MeshBasicMaterial()
-        textMaterial.wireframe = true
-        const textMesh = new THREE.Mesh(textGeometry, textMaterial)
-        textMesh.position.x = 1
-        scene.add(textMesh)
-    }
-)
-```
-
-## Animate - 旋转动画
-```JS
-// Clock
-const clock = new THREE.Clock()
-
-// Animations
-const tick = () => {
-    // Clock
-    const elapsedTime = clock.getElapsedTime()
-    
-    mesh.position.x = Math.cos(elapsedTime)
-    mesh.position.y = Math.sin(elapsedTime)
-
-    // Render
-    renderer.render(scene, camera)
-
-    window.requestAnimationFrame(tick)
-}
-```
-
-# GUI
-
-## 快捷键
-```
-h - 切换开关
-```
-设置gui.hide()隐藏之后需要按两下`h`来唤醒。
--- src/script.js
+++ src/script.js
@@ -1,8 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
 import * as dat from 'lil-gui'
 
 /**
@@ -18,61 +16,50 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Textures
+ * Lights
  */
-const textureLoader = new THREE.TextureLoader()
-const matcapTexture = textureLoader.load('textures/matcaps/8.png')
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
+
+const pointLight = new THREE.PointLight(0xffffff, 0.5)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
 
 /**
- * Fonts
+ * Objects
  */
-const fontLoader = new FontLoader()
+// Material
+const material = new THREE.MeshStandardMaterial()
+material.roughness = 0.4
 
-fontLoader.load(
-    '/fonts/helvetiker_regular.typeface.json',
-    (font) =>
-    {
-        // Material
-        const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
-
-        // Text
-        const textGeometry = new TextGeometry(
-            'Hello Three.js',
-            {
-                font: font,
-                size: 0.5,
-                height: 0.2,
-                curveSegments: 12,
-                bevelEnabled: true,
-                bevelThickness: 0.03,
-                bevelSize: 0.02,
-                bevelOffset: 0,
-                bevelSegments: 5
-            }
-        )
-        textGeometry.center()
-
-        const text = new THREE.Mesh(textGeometry, material)
-        scene.add(text)
-
-        // Donuts
-        const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 32, 64)
-
-        for(let i = 0; i < 100; i++)
-        {
-            const donut = new THREE.Mesh(donutGeometry, material)
-            donut.position.x = (Math.random() - 0.5) * 10
-            donut.position.y = (Math.random() - 0.5) * 10
-            donut.position.z = (Math.random() - 0.5) * 10
-            donut.rotation.x = Math.random() * Math.PI
-            donut.rotation.y = Math.random() * Math.PI
-            const scale = Math.random()
-            donut.scale.set(scale, scale, scale)
-
-            scene.add(donut)
-        }
-    }
+// Objects
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 32, 32),
+    material
 )
+sphere.position.x = - 1.5
+
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(0.75, 0.75, 0.75),
+    material
+)
+
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.3, 0.2, 32, 64),
+    material
+)
+torus.position.x = 1.5
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(5, 5),
+    material
+)
+plane.rotation.x = - Math.PI * 0.5
+plane.position.y = - 0.65
+
+scene.add(sphere, cube, torus, plane)
 
 /**
  * Sizes
@@ -128,6 +115,15 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    sphere.rotation.y = 0.1 * elapsedTime
+    cube.rotation.y = 0.1 * elapsedTime
+    torus.rotation.y = 0.1 * elapsedTime
+
+    sphere.rotation.x = 0.15 * elapsedTime
+    cube.rotation.x = 0.15 * elapsedTime
+    torus.rotation.x = 0.15 * elapsedTime
 
     // Update controls
     controls.update()
